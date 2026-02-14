63e07db9f2906a6e05c86d44c2972e551bfadcb9
behhruz05 <sotimboyevbehruz85@gmail.com>
2026-02-14 15:56:07 +0500

    done

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,4 @@
 import express from "express";
-import { register, login } from "../controllers/auth.controller.js";
 import { register, login, getMe } from "../controllers/auth.controller.js";
 import { protect } from "../middleware/auth.middleware.js";
 
